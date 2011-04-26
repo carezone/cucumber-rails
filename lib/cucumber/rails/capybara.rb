272ae4d75794a2e239e139b509849478d29ba348
Jon Larkowski <jonlarkowski@gmail.com>
2011-04-26 12:33:05 -0700

    Disable JavaScript emulation feature
    Causes capybara 1.0.0.beta1 to fail.  And we don't make use of this
    feature in our test suite.

--- lib/cucumber/rails/capybara.rb
+++ lib/cucumber/rails/capybara.rb
@@ -2,6 +2,11 @@ if defined?(Capybara)
   require 'capybara/rails'
   require 'capybara/cucumber'
   require 'capybara/session'
-  require 'cucumber/rails/capybara/javascript_emulation'
+
+  # Causes capybara 1.0.0.beta1 to fail.  And we don't make use of this feature
+  # in our test suite.
+  #
+  # require 'cucumber/rails/capybara/javascript_emulation'
+
   require 'cucumber/rails/capybara/select_dates_and_times'
 end
